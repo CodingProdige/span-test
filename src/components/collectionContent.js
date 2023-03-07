49da0a896f22d32cf86dda319975f2ccb197e810
Dillon Jurgens <dillon.jurgens@hyvemobile.co.za>
2023-03-07 21:12:37 +0200

    keyboard navigation fully functional

--- src/components/collectionContent.js
+++ src/components/collectionContent.js
@@ -13,8 +13,14 @@ const api = createApi({
 export default function CollectionContent(props) {
     const [collectionData, setCollectionData] = useState([]);
     const [imagesLoaded, setImagesLoaded] = useState(false);
-    let selectedTab = props.selectedTab;
+    const [translateX, setTranslateX] = useState(0);
+    const [selectedTab, setSelectedTab] = useState(0)
+    const [activeImage, setActiveImage] = useState('');
 
+
+    /**
+     * Handle keyboard navigation going from collections to slides
+     */
     document.addEventListener('keydown', (event) => {
         const focusedElement = document.activeElement;
         const carousel = document.getElementById('carousel');
@@ -42,23 +48,34 @@ export default function CollectionContent(props) {
         }
     })
 
+    /**
+     * API Call for collection data
+     */
     useEffect(() => {
         // Reset collectionData before new fetch
         setCollectionData([]);
+        // Set fetched state to false, then updated to true when fetched
         setImagesLoaded(false);
 
         api.collections.getPhotos({ collectionId: `${props.collectionId}`, perPage: 36 })
         .then((data) => {
             setCollectionData(data.response.results)
             setImagesLoaded(true);
+            setSelectedTab(props.selectedTab);
+            setTranslateX(0);
         })
         .catch((error) => console.log(error));
     }, [props]);
 
+
+    /**
+     * Handle keyboard navigation when slides are active
+     */
     useEffect (() => {
         const carousel = document.getElementById('carousel');
         const slides = Array.from(carousel.querySelectorAll('.slide'));
         let activeSlideIndex = 0;
+
     
         // function to update the active slide
         function setActiveSlide(index) {
@@ -67,16 +84,28 @@ export default function CollectionContent(props) {
             });
             slides[index].classList.add('active');
             activeSlideIndex = index;
+            setActiveImage(`${slides[index].getAttribute('data-url')}`);
         }
     
         // event listener for remote control input
         document.addEventListener('keydown', (event) => {
+            const currentItem = slides[activeSlideIndex];
             switch (event.key) {
                 case 'ArrowLeft':
-                    setActiveSlide((activeSlideIndex - 1 + slides.length) % slides.length);
+                    if(activeSlideIndex >= 1) {
+                        setActiveSlide((activeSlideIndex - 1 + slides.length) % slides.length);
+                        setTranslateX(-25 * activeSlideIndex); // 25 represents percentage
+                        currentItem.focus();
+                        currentItem.click();
+                    }
                 break;
                 case 'ArrowRight':
-                    setActiveSlide((activeSlideIndex + 1) % slides.length);
+                    if(activeSlideIndex != slides.length - 1) {
+                        setActiveSlide((activeSlideIndex + 1) % slides.length);
+                        setTranslateX(-25 * activeSlideIndex); // 25 represents percentage
+                        currentItem.focus();
+                        currentItem.click();
+                    }
                 break;
             }
         });    
@@ -84,7 +113,14 @@ export default function CollectionContent(props) {
 
     return (
         <div id="carousel-container">
-            <div id="carousel">
+            <div className="image-in-view-container">
+                <div
+                    className="image-in-view" 
+                    style={{backgroundImage: `url('${activeImage}')`}} 
+                ></div>
+            </div>
+
+            <div id="carousel" style={{transform: `translatex(${translateX}%)`}}>
                 {
                     Object.keys(collectionData).length < 1 ? (
                         <p>Loading...</p>
@@ -96,6 +132,7 @@ export default function CollectionContent(props) {
                                     style={{backgroundImage: `url(${item.urls.thumb})`}}
                                     key={item.id}
                                     tabIndex={index}
+                                    data-url={item.urls.thumb}
                                 >
                                 </li>
                             )
