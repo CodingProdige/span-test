7d14a3e6e92512e4b36b39af8ba9ca6045758331
Dillon Jurgens <dillon.jurgens@hyvemobile.co.za>
2023-03-07 10:28:04 +0200

    Basic keyboard navigation added, no slide motion

--- src/components/collectionContent.js
+++ src/components/collectionContent.js
@@ -12,13 +12,47 @@ const api = createApi({
 
 export default function CollectionContent(props) {
     const [collectionData, setCollectionData] = useState([]);
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+    let selectedTab = props.selectedTab;
+
+    document.addEventListener('keydown', (event) => {
+        const focusedElement = document.activeElement;
+        const carousel = document.getElementById('carousel');
+        const images = Array.from(carousel.querySelectorAll('.slide'));
+      
+    
+        switch (event.key) {
+          case 'RightArrow': 
+            if( focusedElement.classList.contains('collections-list-item') ) {
+              // simulate click when item is selected
+              const currentItem = images[0];
+              currentItem.focus();
+              currentItem.click();
+            }
+          break;
+          case 'LeftArrow': 
+            if( focusedElement.classList.contains('slide') && focusedElement.getAttribute(selectedTab) === 0 ) {
+              // simulate click when item is selected
+              const collection = document.getElementById(`collection_${selectedTab}`);
+              const currentItem = collection;
+              currentItem.focus();
+              currentItem.click();
+            }
+          break;
+        }
+    })
 
     useEffect(() => {
+        // Reset collectionData before new fetch
+        setCollectionData([]);
+        setImagesLoaded(false);
+
         api.collections.getPhotos({ collectionId: `${props.collectionId}`, perPage: 36 })
-        .then((data) => setCollectionData(data.response.results))
+        .then((data) => {
+            setCollectionData(data.response.results)
+            setImagesLoaded(true);
+        })
         .catch((error) => console.log(error));
-        console.log(props.collectionId);
-        console.log(collectionData);
     }, [props]);
 
     useEffect (() => {
@@ -39,30 +73,34 @@ export default function CollectionContent(props) {
         document.addEventListener('keydown', (event) => {
             switch (event.key) {
                 case 'ArrowLeft':
-                setActiveSlide((activeSlideIndex - 1 + slides.length) % slides.length);
+                    setActiveSlide((activeSlideIndex - 1 + slides.length) % slides.length);
                 break;
                 case 'ArrowRight':
-                setActiveSlide((activeSlideIndex + 1) % slides.length);
+                    setActiveSlide((activeSlideIndex + 1) % slides.length);
                 break;
             }
         });    
-    },[]);
+    },[imagesLoaded]);
 
     return (
         <div id="carousel-container">
             <div id="carousel">
                 {
-                    collectionData.map((item, index) => {
-                        return(
-                            <li 
-                                className="slide" 
-                                id={item.id}
-                                tabIndex={index}
-                                style={{backgroundImage: `url(${item.urls.thumb})`}}
-                            >
-                            </li>
-                        )
-                    }) 
+                    Object.keys(collectionData).length < 1 ? (
+                        <p>Loading...</p>
+                    ) : (
+                        collectionData.map((item, index) => {
+                            return(
+                                <li 
+                                    className="slide" 
+                                    style={{backgroundImage: `url(${item.urls.thumb})`}}
+                                    key={item.id}
+                                    tabIndex={index}
+                                >
+                                </li>
+                            )
+                        })
+                    )
                 }
             </div>
         </div>
--- src/components/collections.js
+++ src/components/collections.js
@@ -3,56 +3,127 @@ import { createApi } from "unsplash-js";
 import '../css/collections.css';
 import CollectionContent from './collectionContent';
 
+
+/**
+ * Unsplash API Connection
+ */
 const api = createApi({
-  // Don't forget to set your access token here!
   // See https://unsplash.com/developers
   accessKey: "qC1c2n8GBb0rVl4ZtKjnlGyqYrOrY1uY11nTfxep2mo"
 });
+
+
 
 function UnsplashCollection() {
   const [collectionItems, setCollectionItems] = useState([]);
   const [selectedTab, setSelectedTab] = useState(0);
   const [collectionId, setCollectionId] = useState(null);
+  const [contentLoaded, setContentLoaded] = useState(false);
 
+
+  /**
+   * Mouse click Manual + Emulated
+   * @param {* collection item index } index 
+   * @param {* collection item unsplash id } id 
+   */
   const handleClick = (index, id) => {
     setSelectedTab(index);
-    setCollectionId(id);
+    setCollectionId(id);    
+    console.log(id); 
   };
 
+
+
+  /**
+   * API Collection Call
+   * Page: 2
+   * Limit: 10
+   */
   useEffect(() => {
     api.collections.list({ page: 2, perPage: 10 })
     .then((data) => {
-        setCollectionItems(data.response.results)
-        setCollectionId(data.response.results[0].id)
-        console.log(data.response.results[0].id)
+      setCollectionItems(data.response.results);
+      setCollectionId(data.response.results[0].id);
+      setContentLoaded(true);
     })
     .catch((error) => console.log(error));
   }, []);
 
+
+  /**
+   * Handle Remote Input
+   * Up + Down + Enter
+   */
+  useEffect (() => {
+    // Collections array
+    const list = document.getElementById('collectionList');
+    const items = Array.from(list.querySelectorAll('.collections-list-item'));
+    let activeItemIndex = 0;
+
+    // function to update the active slide
+    function setActiveItem(index) {
+      console.log(index);
+      items.forEach((item) => {
+          item.classList.remove('active');
+      });
+      items[index].classList.add('active');
+      activeItemIndex = index;
+
+      // simulate click when item is selected
+      const currentItem = items[index];
+      currentItem.focus();
+      currentItem.click();
+    }
+   
+
+
+
+    // event listener for remote control input
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case 'ArrowUp':
+              setActiveItem((activeItemIndex - 1 + items.length) % items.length);
+            break;
+            case 'ArrowDown':
+              setActiveItem((activeItemIndex + 1) % items.length);
+            break;
+        }
+    });    
+  },[contentLoaded]);
+
+
   return (
     <div className='main-container'>
         <div className='collections-list-container'>
-            <ul>
-                {
-                    collectionItems && collectionItems.map((item, index) => (
-                        <li 
-                            className={selectedTab === index ? 'active collections-list-item' : 'collections-list-item'}
-                            key={item.id} 
-                            tabIndex={index}
-                            onClick={() => handleClick(index, item.id)}
-                        >
-                            <p className='collections-list-item-title'>{item.title}</p>
-                            <div className='collections-list-item-background' style={{backgroundImage: `url(${item.cover_photo.urls.thumb})`}}>
-                                <div className='opacity-layer'></div>
-                            </div>
-                        </li>
-                    ))
-                }
+            <ul id='collectionList'>
+              {
+                collectionItems && collectionItems.map((item, index) => (
+                  <li 
+                    id={`collection_${index}`}
+                    className={selectedTab === index ? 'active collections-list-item' : 'collections-list-item'}
+                    key={item.id} 
+                    tabIndex={index}
+                    onClick={() => handleClick(index, item.id)}
+                  >
+                    <p className='collections-list-item-title'>{item.title}</p>
+                    <div className='collections-list-item-background' style={{backgroundImage: `url(${item.cover_photo.urls.thumb})`}}>
+                      <div className='opacity-layer'></div>
+                    </div>
+                  </li>
+                ))
+              }
             </ul>
         </div>
         
         <div className='collections-carousel-container'>
-            <CollectionContent collectionId={collectionId}/>
+          {
+            collectionId && (
+              <CollectionContent 
+                collectionId={collectionId}
+                selectedTab={selectedTab}
+              />
+            )
+          }
         </div>
 
     </div>
